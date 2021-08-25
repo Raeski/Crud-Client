4340c7696e97924d8c7441cd85b2addb488daa35
Raeski <gustavoraeski@outlook.com>
2021-08-25 14:54:41 -0300

    Adicionando anotação @Repository no ClientRepository

--- src/main/java/br/com/client/repository/ClientRepository.java
+++ src/main/java/br/com/client/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package br.com.client.repository;
 
 import br.com.client.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
