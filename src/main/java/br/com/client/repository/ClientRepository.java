bbf5826a3fe035441354992980114e473ffd82f9
Raeski <gustavoraeski@outlook.com>
2021-08-25 13:59:55 -0300

    Removendo imports não utilizados

--- src/main/java/br/com/client/repository/ClientRepository.java
+++ src/main/java/br/com/client/repository/ClientRepository.java
@@ -2,10 +2,6 @@ package br.com.client.repository;
 
 import br.com.client.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
-  Client findByName (String name);
-
 }
