f84241ef6ef49f533c82f2b9452c48e03324b20a
Raeski <gustavoraeski@outlook.com>
2021-08-25 13:38:40 -0300

    Criando serviço para atualizar cliente e refatoração da classe Service

--- src/main/java/br/com/client/controller/ClientController.java
+++ src/main/java/br/com/client/controller/ClientController.java
@@ -3,6 +3,7 @@ package br.com.client.controller;
 import br.com.client.model.Client;
 import br.com.client.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +18,22 @@ public class ClientController {
 
   @PostMapping
   public ResponseEntity<Client> save(@RequestBody Client client) {
-    return clientService.saveClient(client);
+    return new ResponseEntity(clientService.saveClient(client), HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Client> findById(@PathVariable("id") Long id ){
-    return clientService.findById(id);
+    return new ResponseEntity(clientService.findById(id),HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
   public String delete(@PathVariable("id") Long id) {
     return clientService.delete(id);
+  }
+
+  @PostMapping("/replace")
+  public ResponseEntity<Client> updateClient(@RequestBody Client client) {
+    return new ResponseEntity(clientService.replace(client), HttpStatus.OK);
   }
 
 }
--- src/main/java/br/com/client/service/ClientService.java
+++ src/main/java/br/com/client/service/ClientService.java
@@ -3,9 +3,9 @@ package br.com.client.service;
 import br.com.client.model.Client;
 import br.com.client.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientService{
@@ -13,17 +13,23 @@ public class ClientService{
   @Autowired
   ClientRepository clientRepository;
 
-  public ResponseEntity<Client> saveClient(Client client){
-    return new ResponseEntity(clientRepository.save(client), HttpStatus.CREATED);
+  public Client saveClient(Client client){
+    return clientRepository.save(client);
   }
 
-  public ResponseEntity<Client> findById(Long id) {
-    return new ResponseEntity(clientRepository.findById(id), HttpStatus.OK);
+  public Optional<Client> findById(Long id) {
+    return clientRepository.findById(id);
   }
 
   public String delete(Long id) {
     clientRepository.deleteById(id);
     return "removido com sucesso";
+  }
+
+  public Client replace(Client client){
+    Optional<Client> byId = findById(client.getId());
+    client.setId(byId.get().getId());
+    return clientRepository.save(client);
   }
 
 
